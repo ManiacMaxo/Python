0d04471ac115b396b447b956de59348ced3e8341
Victor <victor@gorchilov.com>
2019-10-10 11:49:51 +0300

    TP

--- inheritance.py
+++ inheritance.py
@@ -3,13 +3,52 @@ class Address:
         self.__street = street
         self.__city = city
         self.__number = number
-        print(number)
+
+    @property
+    def street(self):
+        return self.__street
+
+    @street.setter
+    def street(self, value):
+        self.__street = value
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, value):
+        self.__city = value
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, value):
+        self.__number = value
 
 
 class Person:
     def __init__(self, name, street, city, number):
         self.__name = name
         self.__address = Address(street, city, number)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def address(self):
+        return self.__address
+
+    @address.smetter
+    def address(self, value):
+        self.__address = value
 
 
 class Markbook:
@@ -22,7 +61,6 @@ class Markbook:
 
     def get_marks(self, subject):
         self.__grades[subject]
-        
 
     def get_average(self, subject):
         return sum(self.__grades[subject]) / len(self.__grades[subject])
@@ -37,7 +75,79 @@ class Computer:
         self.__ram = ram
         self.__memory = memory
 
+    @property
+    def brand(self):
+        return self.__brand
+
+    @brand.setter
+    def brand(self, value):
+        self.__brand = value
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, value):
+        self.__model = value
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value):
+        self.__year = value
+
+    @property
+    def cpu(self):
+        return self.__cpu
+
+    @cpu.setter
+    def cpu(self, value):
+        self.__cpu = value
+
+    @property
+    def ram(self):
+        return self.__ram
+
+    @ram.setter
+    def ram(self, value):
+        self.__ram = value
+
+    @property
+    def memory(self):
+        return self.__memory
+
+    @memory.setter
+    def memory(self, value):
+        self.__memory = value
+
 
 class Student(Person):
     def __init__(self):
-        self.number = input('Enter number of student: ')
+        self.__number = input('Enter number of student: ')
+        self._init_computer()
+
+    def _init_computer(self):
+        properties = ['brand', 'model', 'year', 'os', 'cpu', 'ram', 'memory']
+        args = []
+        for property in properties:
+            args.append(input('Enter ' + property))
+
+        self.__computer = Computer(*args)
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, value):
+        self.__number = value
+
+
+if __name__ == '__main__':
+    st = Student()
+    print(st.number)
+    st.number = 42
+    print(st.number)
