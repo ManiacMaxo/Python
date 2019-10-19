da1bce22017d8b33d2f1e03676716a205085b42e
Victor <victor@gorchilov.com>
2019-10-19 13:09:25 +0300

    ml

--- nearest_nb.py
+++ nearest_nb.py
@@ -1,18 +1,21 @@
 from math import sqrt
+import random
 D = 2  # dimensions
 
 
 class Point:
     __coords = []
+    __bool = bool()
 
-    def __init__(self, D, arr):
+    def __init__(self, arr, tof):
         self.__coords = arr
+        __bool = tof  # tof = true of false
 
 
 def euclidean_dist(D, p1, p2):
     sum = float
     for i in range(D):
-        sum += p1.coords[i] + p2.coords[i]
+        sum += pow((p2.coords[i] - p1.coords[i]), 2)
     return sqrt(sum)
 
 
@@ -29,9 +32,15 @@ def nearest_point(D, graph, val):
 
 
 def main():
-    graph = []
-    val = Point(D, (20, 1))
-    nearest_point(2, graph, val)
+    print('program running')
+    # dimensions = int(input('dimensions: '))
+    tof = [True, False]
+    dataset = []
+    for i in range(10):
+        coords = []
+        for j in range(D):  # x, y, z... n-dimensions
+            coords.append(random.uniform(1, 50))
+        dataset.append(Point(coords, tof[random.randint(0, 1)]))
 
 
 main()
