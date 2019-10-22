145ae863eb294d484b1ea465756417c36357f70b
Victor <victor@gorchilov.com>
2019-10-22 09:47:42 +0300

    ml and jupyter

--- nearest_nb.py
+++ nearest_nb.py
@@ -4,34 +4,28 @@ D = 2  # dimensions
 
 
 class Point:
-    __coords = []
-    __bool = bool()
-
     def __init__(self, arr, tof):
-        self.__coords = arr
-        __bool = tof  # tof = true of false
+        self.coords = arr
+        self.bool = tof  # tof = true of false
 
 
 def euclidean_dist(D, p1, p2):
-    sum = float
+    sum = 0
     for i in range(D):
-        sum += pow((p2.coords[i] - p1.coords[i]), 2)
+        sum += (p2.coords[i] - p1.coords[i]) ** 2
     return sqrt(sum)
 
 
-def nearest_point(D, graph, val):
-    min = 1000000
-    for point in graph:
-        dist = euclidean_dist(D, val, point)
-        if dist < min:
-            min = dist
-            p = point
-            if min == 0:
-                break
-    return p
+def distances(D, graph, new):
+    dist = {}
+    for i in range(len(graph)):
+        dist[i] = euclidean_dist(D, graph[i], new)
+
+    dist = sorted(dist.items(), key=lambda item: item[1])
+    print(dist)
 
 
-def main():
+if __name__ == '__main__':
     print('program running')
     # dimensions = int(input('dimensions: '))
     tof = [True, False]
@@ -41,6 +35,4 @@ def main():
         for _ in range(D):  # x, y, z... n-dimensions
             coords.append(random.uniform(1, 50))
         dataset.append(Point(coords, tof[random.randint(0, 1)]))
-
-
-main()
+    distances(D, dataset, Point((1, 2), False))
