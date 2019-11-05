6428b2e60f47eb6e56f8a79c1889a2079e9b763e
Victor <victor@gorchilov.com>
2019-11-05 10:06:12 +0200

    ml and generator

--- prime_generator.py
+++ prime_generator.py
@@ -1,18 +1,29 @@
 from math import sqrt
+from random import randint as rand
 
-def prime_generator():
-	count = 3
-	while True:
-		prime = True
-		for num in range(2, int(sqrt(count) + 1)):
-			if count % num == 0: 
-				prime = False
-				break
-		if prime:
-			print(count)
-		count += 1
+
+def prime_generator(max):
+    prime = 1
+    count = 3
+
+    if max == 1:
+        print('1')
+        exit()
+    while count < max:
+        while True:
+            prime += 1
+            is_prime = True
+            for i in range(2, prime):
+                if prime % i == 0 and prime != i:
+                    is_prime = False
+                    break
+            if is_prime == True:
+                yield prime
+                count += 1
+                break
 
 
 if __name__ == "__main__":
     print('starting')
-    prime_generator()
+    for num in prime_generator(10):
+        print(num)
