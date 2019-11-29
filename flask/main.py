20630bd218101d9bc16bf781ac77b87285a97863
Victor <victor@gorchilov.com>
2019-11-29 08:53:21 +0200

    flask

--- flask/main.py
+++ flask/main.py
@@ -1,22 +1,35 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from post import Post
 
 app = Flask(__name__)
 
-db = [('Post 1', 'Ivan', 'Content 1'), ('Post 2', 'Gosho', 'Content 2')]
+db = {1: Post(1, 'Post 1', 'Gosho', 'Content 1')}
 
 
 @app.route('/')
 def hello_world():
-    return 'hello world'
+    return 'Hello, World!!!'
 
 
 @app.route('/posts')
 def list_posts():
-    return render_template('posts.html', posts=db)
+    return render_template('posts.html', posts=db.values())
 
 
 @app.route('/posts/<int:id>')
-def get_post(id):
+def show_post(id):
     post = db[id]
-    return render_template('posts.html', posts=post)
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'GET':
+        return render_template('new_post.html')
+    elif request.method == 'POST':
+        next_id = max(db.keys()) + 1
+        post = Post(
+            next_id, request.form['name'], request.form['author'], request.form['content'])
+        db[next_id] = post
+        return redirect(url_for('list_posts'))
